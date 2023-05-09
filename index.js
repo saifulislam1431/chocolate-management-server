c8b3cfff6ff360da79a3f3f03c02aad8dc79c9af
Saiful Islam <sakibbhaiya143@gmail.com>
2023-05-10 02:30:03 +0600

    Got Database collection and server setup

---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require('express');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const cors = require('cors');
+const port = process.env.PORT || 6000;
+const app = express();
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.8cnv71c.mongodb.net/?retryWrites=true&w=majority`;
+
+app.use(cors());
+app.use(express.json())
+
+
+
+
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    
+    await client.connect();
+
+    const chocolateCollection = client.db("chocolateServer").collection("chocolates");
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+app.get("/",(req,res)=>{
+    res.send("This is chocolate server");
+})
+
+
+app.listen(port , ()=>{
+console.log(`This server listen at port: ${port}`);
+})
